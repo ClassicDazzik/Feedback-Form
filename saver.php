1382666ff13bf4cedb63534ebb22bc29bee1c078
ClassicDazzik <58544514+ClassicDazzik@users.noreply.github.com>
2022-01-25 12:52:09 +0200

    Completely Revamped the Code
    Decided to start working on this again

--- saver.php
+++ saver.php
@@ -1,12 +1,37 @@
 <?php
+
+// Syntaxes
 $TheForm = fopen("index.html", "r") or die("Failed to open file.");
+$title = $_POST['title'];
+$email = $_POST['email'];
+$feedback = $_POST['feedback'];
+
+// Save Form Responses into Txt file
+
+$file=fopen("result.txt", "a");
+$write=("<p>$title<br>$email<br>$feedback</p>");
+fwrite($file, $write);
+fclose($file);
+
+// Email (I didnt have mailserver setup so i didnt test this)
+$mesSubject = 'Feedback Received!';
+$mes = 'Hello! Thank you for the feedback.';
+$message = mail ($email, $mesSubject, $mes)
 ?>
+
+<!DOCTYPE html>
 <html>
+<head>
+<title>Feedback Page</title>
+</head>
 <body>
 
-Thanks for your feedback!
-Your title: <?php echo $_GET["title"]; ?><br>
-Feedback: <?php echo $_GET["feedback"]; ?>
+<h1>Thank you! Here are your responses</h1>
+<?php
+echo "Your email is " . $email . "<br>";
+echo "Feedback's Title:" . $title . "<br>";
+echo "Submitted Feedback: " . $feedback . "<br>";
+?>
 
 </body>
 </html>
